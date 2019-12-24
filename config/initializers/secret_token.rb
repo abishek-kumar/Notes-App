baab5b52fb67ff23a2e333346d1cf6a918733ea5
Abishek Kumar S <askumar@FWS-CHE-LT-3273.local>
2019-12-24 13:02:18 +0530

    Project structure created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotesApp::Application.config.secret_token = 'bc3f4c26ce5374257e5c6b82d0c1db8f699b45be8ce85c69cdaec2d7ea6538214161651fc327be0abc9d8545e1503d0e812147e538144594179de030df7a2049'
